f4981d574f830ac2d49e00659afd1edc1b9e4cd5
GirishV94 <30563161+GirishV94@users.noreply.github.com>
2017-08-20 08:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,17 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "NewValue";
+
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+    }
+img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
+    img.style.marginleft = '100px';
+};
+
+
