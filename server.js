97698f226fe32728b2cc58fca1cd81fa68c61a7e
GirishV94 <30563161+GirishV94@users.noreply.github.com>
2017-08-25 20:36:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,11 +99,10 @@ app.get('/test-db', function(req,res) {
     //make a select request
     //return a response with the results
     pool.query('SELECT * FROM test', function(err,result){
-    if(err)    {
+    if(err){
         res.status(500).send(err.toString());
-    } else
-    {
-       res.send(JSON.(stringify(result));
+    } else{
+       res.send(JSON.stringify(result));
     }
     });
 });
