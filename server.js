fc0ff178f6edf75fa930400fb4771048f876daf3
GirishV94 <30563161+GirishV94@users.noreply.github.com>
2017-08-27 10:58:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/articles/:articleName',function (req,res) {
                     res.status(404).send('Article Not Found');
                  }else{
                     var articleData = result.rows[0];        
-                    res.send(createTemplate(articlesData));
+                    res.send(createTemplate(articleData));
                     }
             }
     });
