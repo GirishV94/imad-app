8796e12bb50fa0424eb7c6d630da69b77bd6ad88
GirishV94 <30563161+GirishV94@users.noreply.github.com>
2017-08-31 21:33:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,3 @@
-//counter code
-var button = document.getElementById('counter');
-
-button.onclick = function() {
-    
-    // create the request object
-    var request = new XMLHttpRequest();
-   
-   
-    // cap res and store in var
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }//Note Done Yet
-    };
-    //make a response to counter var
-    request.open('GET', 'http://gvidhani99.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
 
 //submit name
 var submit = document.getElementById('submit_btn');
@@ -37,24 +12,25 @@ submit.onclick=function(){
             //Take some action
             if(request.status === 200){
                 //capture a list of names and render it as a list.
-                    var names =request.responseText;
-                    names = JSON.parse(names)
-                    var list = '';
-                    for (var i=0; i< names.length; i++){
-                        list += '<li>' + names[i] + '</li>';
-                    }
-                    var ul = document.getElementById('namelist');
-                    ul.innerHTML = list;
-    
-            }
+                    console.log('user log in');
+                    alert('logged in successfuly');
+                }else if(request.status === 403){
+                    alert('Username/Password is Incorrect');
+                }else if(request.status === 500){
+                    alert('Something went wrong on the Server');
+                }
+            
         }//Note Done Yet
     };
     //make a response to counter var
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://gvidhani99.imad.hasura-app.io/submit-name?name=' +name, true);
-    request.send(null);
+    var  username = document.getElementById('username').value;
+    var  password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://gvidhani99.imad.hasura-app.io/login', true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 
 };
 
