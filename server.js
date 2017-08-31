85483b3389c16b9e3a67b8b753423a5e6c6a2cf1
GirishV94 <30563161+GirishV94@users.noreply.github.com>
2017-08-31 21:58:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,6 +132,11 @@ app.get('/check-login',function(req,res){
        res.send('You are not logged in');
    }
  });
+ 
+ app.get('/logout', function(req,res){
+     delete req.session.auth;
+     res.send('Logged out');
+ });
 
 
 function createTemplate(data){
